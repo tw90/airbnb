c9697ce1962d0271020ebd5c506c5291a9bbe7c3
tw90 <wong.teng.wei@outlook.com>
2016-09-29 11:33:18 +0800

    added mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,21 @@ Rails.application.configure do
 
   Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
 
+  #mailer setting
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:                'smtp.gmail.com',
+    port:                   587,
+    # domain:                 'localhost:3000',
+    user_name:              ENV["GMAIL_USERNAME"],
+    password:               ENV["GMAIL_PASSWORD"],
+    authentication:         'plain',
+    enable_starttls_auto:   true
+
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'nextairbnb@gmail.com' }
+
 end
